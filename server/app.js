1b6148cb17230a2f6cfb949472f835ca7f755012
Argishti Karapetyan <karapetyana@webtrends.com>
2017-07-10 12:24:01 +0400

    initial commit

---
+++ server/app.js
@@ -0,0 +1,11 @@
+// server/app.js
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.get('*', (req, res) => {
+    //TODO
+});
+
+module.exports = app;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Login from './components/Login/index';
+
+ReactDOM.render(<Login />, document.getElementById('root'));
